fd40d255686546e5146d12abe6b23c65e959b057
harrylae <harry_lae@hotmail.com>
2021-02-15 20:16:48 -0300

    Agregado de Skin Graystyle
    Se agrega skin y ademas se agrega mapas de leafletjs.com

---
+++ red-bicicletas/public/javascripts/map.js
@@ -0,0 +1,5 @@
+var mymap = L.map('main_map').setView([-32.8967174,-68.836055], 13);
+
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(mymap);
