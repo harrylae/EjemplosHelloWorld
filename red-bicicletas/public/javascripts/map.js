e25d1ec8ad0b8aa3e847212eeb4e374764c1b976
harrylae <harry_lae@hotmail.com>
2021-02-21 18:55:02 -0300

    API
    Terminada la API

--- red-bicicletas/public/javascripts/map.js
+++ red-bicicletas/public/javascripts/map.js
@@ -3,3 +3,15 @@ var mymap = L.map('main_map').setView([-32.8967174,-68.836055], 13);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
+
+$.ajax({
+    dataType: "json",
+    url: "API/bicicletas",
+    success: function(result){
+        console.log(result);
+
+        result.bicicletas.forEach(function(bici){
+            L.marker(bici.ubicacion,{title: bici.id}).addTo(mymap)
+        });
+    }
+})
